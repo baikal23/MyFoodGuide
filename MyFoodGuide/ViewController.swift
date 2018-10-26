5405468827d97819b994a4c94a1cdee50aa64f42
skohler00 <skohler00@gmail.com>
2018-10-26 08:42:42 -0400

    Initial Commit

---
+++ MyFoodGuide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFoodGuide
+//
+//  Created by Susan Kohler on 10/26/18.
+//  Copyright © 2018 Susan Kohler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
